7c7ec4d45ab277f3407dc588b30445146161a697
keerthimac <keerthi.mac@gmail.com>
2022-04-05 20:59:42 +0530

    app.css name changed

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,21 @@ function App() {
   const userEmailRef = useRef("");
   const firstNameRef = useRef("");
   const lastNameRef = useRef("");
-  
-  
+
   const onFromSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     // `current` points to the mounted text input element
     // usernameRef.current.focus();
-    console.log(usernameRef.current.value)
-    console.log(userEmailRef.current.value)
-    console.log(firstNameRef.current.value)
-    console.log(lastNameRef.current.value)
+    console.log(usernameRef.current.value);
+    console.log(userEmailRef.current.value);
+    console.log(firstNameRef.current.value);
+    console.log(lastNameRef.current.value);
   };
-
 
   return (
     <div className='app'>
       <form onSubmit={onFromSubmit}>
-        <FromInput refer={usernameRef} placeholder='Username'/>
+        <FromInput refer={usernameRef} placeholder='Username' />
         <FromInput refer={userEmailRef} placeholder='Email' />
         <FromInput refer={firstNameRef} placeholder='First Name' />
         <FromInput refer={lastNameRef} placeholder='Last Name' />
